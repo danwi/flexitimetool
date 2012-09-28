a3c80b080d15a0e7105aed728781288e84d36094
Daniel Wiklund <daniel.n.wiklund@gmail.com>
2012-09-28 17:47:07 +0200

    Rewrote IdleTimeUtils to be extendable for more platforms
    The old W32IdleTimeUtils was hardcoded for Windows platform, with the
    new IdleTimeUtils the class can easilly be extended to support more
    platforms without having to change how the class is used.

---
+++ src/main/java/org/flexitimetool/idletime/IdleTimeUtils.java
@@ -0,0 +1,54 @@
+package org.flexitimetool.idletime;
+
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+import com.sun.jna.Structure;
+import com.sun.jna.win32.StdCallLibrary;
+
+public class IdleTimeUtils {
+	
+	private IdleTimeCalculator idleCalculator;
+
+	public IdleTimeUtils() {
+		if (Platform.isWindows()) {
+			idleCalculator = new W32IdleTimeCalculator();
+		} else {
+			throw new RuntimeException("Unsupported platform " + Platform.getOSType());
+		}
+	}
+	
+	long getIdleTimeMillis() {
+		return idleCalculator.getIdleTimeMillis();
+	}
+	
+	private interface IdleTimeCalculator {
+		public long getIdleTimeMillis();
+	}
+	
+	
+	public interface Kernel32 extends StdCallLibrary {
+		Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32",
+				Kernel32.class);
+
+		public int GetTickCount();
+	};
+
+	public interface User32 extends StdCallLibrary {
+		User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class);
+
+		public static class LASTINPUTINFO extends Structure {
+			public int cbSize = 8;
+			public int dwTime;
+		}
+
+		public boolean GetLastInputInfo(LASTINPUTINFO result);
+	};
+
+	public class W32IdleTimeCalculator implements IdleTimeCalculator {
+		public long getIdleTimeMillis() {
+			User32.LASTINPUTINFO lastInput = new User32.LASTINPUTINFO();
+			User32.INSTANCE.GetLastInputInfo(lastInput);
+			return Kernel32.INSTANCE.GetTickCount() - lastInput.dwTime;
+		}
+	}
+}
--- src/test/java/org/flexitimetool/idletime/IdleTimeDemo.java
+++ src/test/java/org/flexitimetool/idletime/IdleTimeDemo.java
@@ -4,14 +4,14 @@ import java.text.MessageFormat;
 
 
 
-public class W32IdleTimeDemo {
+public class IdleTimeDemo {
 
 	public static void main(String arg[]) throws InterruptedException {
 		idleDisplayLoop();
 	}
 
 	private static void idleDisplayLoop() throws InterruptedException {
-		W32IdleTimeUtils idleTimeUtils = new W32IdleTimeUtils();
+		IdleTimeUtils idleTimeUtils = new IdleTimeUtils();
 		while(true) {
 			Thread.sleep(1000);
 			long idleTime = idleTimeUtils.getIdleTimeMillis();
